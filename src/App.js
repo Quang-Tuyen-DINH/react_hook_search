c96189a180c09a4fab581dc010a14f424b1fee34
Quang Tuyen DINH <qtuyen43@gmail.com>
2020-06-30 23:58:56 +0200

    Modify App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-function App() {
-  return (
-    <div className="App">
-      <h2>Hello from App</h2>
+import Header from './components/Header';
+import Movie from './components/Movie';
+import Search from './components/Search';
+import movie_api from './data';
+
+const MOVIE_API_URL = movie_api;
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    fetch(MOVIE_API_URL)
+    .then(response => response.json())
+    .then(jsonResponse => {
+      setMovies(jsonResponse.Search);
+      setLoading(false);
+    });
+  });
+
+  const search = searchValue => {
+    setLoading(true);
+    setErrorMessage(null);
+
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        if (jsonResponse.Response === "True") {
+          setMovies(jsonResponse.Search);
+          setLoading(false);
+        } else {
+          setErrorMessage(jsonResponse.Error);
+          setLoading(false);
+        }
+      });
+  	};
+
+    return (
+     <div className="App">
+      <Header text="HOOKED" />
+      <Search search={search} />
+      <p className="App-intro">Sharing a few of our favourite movies</p>
+      <div className="movies">
+        {loading && !errorMessage ? (
+         <span>loading...</span>
+         ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          movies.map((movie, index) => (
+            <Movie key={`${index}-${movie.Title}`} movie={movie} />
+          ))
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
